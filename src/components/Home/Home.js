afb916cb7f64cd610e40dc59e2ba784f81078a28
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-02-15 22:40:35 +0530

    feat

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,13 @@ import Card from '../UI/Card/Card';
 import Button from '../UI/Button/Button';
 import classes from './Home.module.css';
 
-const Home = (props) => {
+const Home = () => {
   return (
     <Card className={classes.home}>
+      <div className={classes.complete}></div>
       <h1>THANK YOU!</h1>
       <p>We have added your card details</p>
-      <Button>Continue</Button>
+      <Button className={classes.buttonComplete}>Continue</Button>
     </Card>
   );
 };
